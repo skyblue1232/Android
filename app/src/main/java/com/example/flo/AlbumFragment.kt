95f236bfe20427c3ef17fc8afd0f8bf798845d94
londf <qkrrytn99@naver.com>
2024-04-13 14:56:59 +0900

    feat : 2week 과제 제출

--- app/src/main/java/com/example/flo/AlbumFragment.kt
+++ app/src/main/java/com/example/flo/AlbumFragment.kt
@@ -4,26 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.flo.databinding.FragmentHomeBinding
+import com.example.flo.databinding.FragmentAlbumBinding
 
-class HomeFragment : Fragment() {
-
-    private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
+class AlbumFragment : Fragment() {
+    lateinit var binding : FragmentAlbumBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = FragmentAlbumBinding.inflate(inflater,container,false)
 
-        binding.homeMainAlbumImg1Iv.setOnClickListener{
+        binding.albumBackIv.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm,AlbumFragment()).commitAllowingStateLoss()
+        }
 
+        binding.songLalacLayout.setOnClickListener{
+            Toast.makeText(activity,"LILAC", Toast.LENGTH_SHORT).show()
         }
         return binding.root
     }
-
 }
--- app/src/main/java/com/example/flo/MainActivity.kt
+++ app/src/main/java/com/example/flo/MainActivity.kt
@@ -1,18 +1,43 @@
 package com.example.flo
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.flo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    private val getResultText = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){result->
+        if(result.resultCode== Activity.RESULT_OK){
+            val returnString = result.data?.getStringExtra("songsong")
+            Toast.makeText(applicationContext, returnString, Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val song = Song(binding.mainMiniplayerTitleTv.text.toString(), binding.mainMiniplayerSingerTv.text.toString())
+
+        Log.d("Song", "제목 : ${song.title} / 가수 : ${song.singer}")
+
+        binding.mainPlayerCl.setOnClickListener{
+            //startActivity(Intent(this, SongActivity::class.java))
+            val intent = Intent(this,SongActivity::class.java)
+            intent.putExtra("title",song.title)
+            intent.putExtra("singer",song.singer)
+            startActivity(intent)
+        }
         initBottomNavigation()
 
     }
